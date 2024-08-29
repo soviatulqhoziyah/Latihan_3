7a86f132e2c5cedeca884b1e56b613070a7cc98b
soviatulqhoziyah <soviatulqhoziyah13@gmail.com>
2024-08-29 11:08:58 +0700

    Initial commit

---
+++ app/src/main/java/com/soviatul/latihan_3/Acitivity_Table.kt
@@ -0,0 +1,20 @@
+package com.soviatul.latihan_3
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class Acitivity_Table : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_acitivity_table)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
